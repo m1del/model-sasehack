6fda99b2b3346e4d25f459deedfa5049329008c4
John Miclat <jmiclat@ufl.edu>
2023-10-08 06:57:02 -0400

    fix default roberta usage

--- models/defaultroberta.py
+++ models/defaultroberta.py
@@ -1,3 +1,4 @@
+import torch
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 
 tokenizer = AutoTokenizer.from_pretrained("distilroberta-base")
@@ -7,21 +8,24 @@ def predict_expiration(model, tokenizer, food_item, location, temperature):
     # Constructing the input sentence
     input_sentence = (
         f"I am storing {food_item} in my {location} "
-        f"at {temperature}°F, how long until it spoils or goes bad?"
+        f"at {temperature} degrees fahrenheit, it will spoil in <mask> days."
     )
-    
-    # Tokenizing the input sentence
+    # Tokenizing
     inputs = tokenizer(input_sentence, return_tensors='pt', padding=True, truncation=True, max_length=64)
-    
-    # Moving inputs to the same device as the model
     inputs = {name: tensor.to(next(model.parameters()).device) for name, tensor in inputs.items()}
     
     # Get the model output
     with torch.no_grad():
         output = model(**inputs)
     
-    # Convert model output to days (as a single float value)
-    predicted_days = output.item()
+    # Extracting prediction and convert
+    predicted_token_id = torch.argmax(output.logits[0, inputs['input_ids'][0] == tokenizer.mask_token_id])
+    predicted_token = tokenizer.convert_ids_to_tokens([predicted_token_id])[0]
+    predicted_days = ''.join(filter(str.isdigit, predicted_token))
+    
+    # Null checkssss
+    if not predicted_days:
+        predicted_days = "an unknown number of"
     
     return predicted_days
 
@@ -31,7 +35,7 @@ def main():
     temperature = input("Enter temperature: ")
 
     predicted_days = predict_expiration(model, tokenizer, food, location, temperature)
-    print(f"The {food} in the {location} will spoil after approximately {predicted_days:.2f} days.")
+    print(f"The {food} in the {location} will spoil after approximately {predicted_days} days.")
 
 if __name__ == '__main__':
     main()
