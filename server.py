e43c63daf3465794b1410028ce2287dedbf81f7d
John Miclat <jmiclat@ufl.edu>
2023-10-08 19:57:27 -0400

    add endpoint to populate expiry date

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from models.recipe import get_ingredients_to_expire, get_recipe
+from models.defaultroberta import update_expirations_in_firestore
 
 app = Flask(__name__)
 
@@ -13,6 +14,16 @@ def get_recipe_endpoint():
     ingredients, inventory = get_ingredients_to_expire()
     recipe = get_recipe(ingredients, inventory)
     return jsonify({"recipe": recipe})
+
+@app.route("/update_expirations", methods=["POST"])  # Use POST because this endpoint modifies data
+def update_expirations_endpoint():
+    # This endpoint will call the function to update the "daysTillExpire" for each item
+    try:
+        update_expirations_in_firestore()
+        return jsonify({"status": "success", "message": "Expirations updated successfully!"}), 200
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run(port=5000)
